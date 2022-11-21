5737f578d412273e2b086a683af8dee772e39868
meo-s <kmeos1579@gmail.com>
2022-11-21 19:25:00 +0900

    solve: baekjoon #7576

--- 07000/500/7576-토마토.py
+++ 07000/500/7576-토마토.py
@@ -1,1 +1,34 @@
 # https://www.acmicpc.net/problem/7576
+
+import sys
+from collections import deque
+
+readline = lambda: sys.stdin.readline().strip()
+
+M, N = map(int, readline().split())
+
+box = []
+n_raws = 0
+waitings = deque([])
+for row in range(N):
+    box.append([])
+    for col, n in enumerate(map(int, readline().split())):
+        box[-1].append(n)
+        n_raws += 1 if n == 0 else 0
+        waitings.append((row, col)) if n == 1 else None
+
+n_days = 0
+while 0 < n_raws and 0 < len(waitings):
+    for _ in range(len(waitings)):
+        row, col = waitings.popleft()
+        for drow, dcol in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
+            if (not 0 <= row + drow < N) or (not 0 <= col + dcol < M):
+                continue
+            if box[row + drow][col + dcol] == 0:
+                box[row + drow][col + dcol] = 1
+                n_raws -= 1
+                waitings.append((row + drow, col + dcol))
+
+    n_days += 1
+
+print([-1, n_days][n_raws == 0])
