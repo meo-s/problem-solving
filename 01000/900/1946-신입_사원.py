5be558c8716e81f48d9a08f7277ddadb25dd6535
meo-s <kmeos1579@gmail.com>
2022-11-21 16:43:06 +0900

    solve: baekjoon #1946

--- 01000/900/1946-신입_사원.py
+++ 01000/900/1946-신입_사원.py
@@ -1,18 +1,21 @@
 # https://www.acmicpc.net/problem/1946
 
 import sys
-import functools
 
 readline = lambda: sys.stdin.readline().strip()
 
-
-def cmp(v1, v2):
-    if v1[0] == v2[0]:
-        return -1 if v1[1] < v2[1] else 0
-    return -1 if v1[0] < v2[0] else 0
-
-
 for _ in range(int(readline())):
     N = int(readline())
-    people = sorted([tuple(map(int, readline().split())) for _ in range(N)], key=functools.cmp_to_key(cmp))
-    print(people)
+    people = sorted([tuple(map(int, readline().split())) for _ in range(N)], key=lambda v: v[0], reverse=True)
+
+    n_recruits = 0
+    rank_bound = N
+    while 0 < len(people):
+        _, rank = people.pop()
+        if rank_bound < rank:
+            continue
+
+        n_recruits += 1
+        rank_bound = min(rank_bound, rank)
+
+    print(n_recruits)
