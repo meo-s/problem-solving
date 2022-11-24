6062ec0b92e946317d5fd2f90e6f622dbc3a52d6
meo-s <kmeos1579@gmail.com>
2022-11-24 22:20:33 +0900

    solve: baekjoon #11279

---
+++ 11000/200/11279-최대_힙.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/11279
+
+import sys
+from heapq import heappush, heappop
+
+readline = lambda: sys.stdin.readline().strip()
+
+heap = []
+for _ in range(int(readline())):
+    arg = int(readline())
+    if arg == 0:
+        print(-heappop(heap) if 0 < len(heap) else 0)
+    else:
+        heappush(heap, -arg)
