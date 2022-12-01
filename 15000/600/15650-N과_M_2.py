1a209f2b0854e8fa1ad97bb3501bbb101f333377
meo-s <kmeos1579@gmail.com>
2022-12-01 17:40:21 +0900

    solve: baekjoon #15650

---
+++ 15000/600/15650-N과_M_2.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/15650
+
+
+def search(N, M, nums=[0], depth=0):
+    if depth < M:
+        for i in range(nums[-1] + 1, N + 1):
+            nums.append(i)
+            search(N, M, nums, depth + 1)
+            nums.pop()
+    else:
+        print(*nums[1:], sep=' ')
+
+
+search(*map(int, input().split()))
