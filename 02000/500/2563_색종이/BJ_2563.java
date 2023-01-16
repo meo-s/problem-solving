0f22fa6804cd5db0080b043640c513304f3029dc
meo-s <meoseong.ki@gmail.com>
2023-01-17 03:00:52 +0900

    change: directory structure of baekjoon #2563

--- 02000/500/2563_색종이/BJ_2563.java
+++ 02000/500/2563_색종이/BJ_2563.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
-public class Baekjoon_2563 {
+public class BJ_2563 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
