c8501200e9a667953d739e1770dfef437f275fb7
meo-s <meoseong.ki@gmail.com>
2023-02-06 23:48:46 +0900

    refactor: only memorize width of rectangles

--- 01000/600/1687_행렬_찾기/BJ_1687_행렬_찾기.java
+++ 01000/600/1687_행렬_찾기/BJ_1687_행렬_찾기.java
@@ -7,13 +7,13 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class BJ_1687_행렬_찾기 {
-    public static int findMaxArea(final int[][][] dp, final int y, final int x) {
+    public static int findMaxArea(final int[][] dp, final int y, final int x) {
         var maxArea = 0;
 
         var dy = 0;
         var w = Integer.MAX_VALUE;
-        while (0 <= y - dy && 0 < dp[y - dy][x][1]) {
-            w = Math.min(w, dp[y - dy][x][1]);
+        while (0 <= y - dy && 0 < dp[y - dy][x]) {
+            w = Math.min(w, dp[y - dy][x]);
             maxArea = Math.max(maxArea, (dy + 1) * w);
             ++dy;
         }
@@ -41,15 +41,12 @@ public class BJ_1687_행렬_찾기 {
         }
 
         var maxArea = 0;
-        final var dp = new int[H][W][2]; // 0: y, 1: x
+        final var dp = new int[H][W];
         for (var y = 0; y < H; ++y) {
             for (var x = 0; x < W; ++x) {
                 if (m[y][x] == 0) {
-                    dp[y][x][0] = (0 < y ? dp[y - 1][x][0] + 1 : 1);
-                    dp[y][x][1] = (0 < x ? dp[y][x - 1][1] + 1 : 1);
-                    if (maxArea < dp[y][x][0] * dp[y][x][1]) {
-                        maxArea = Math.max(maxArea, findMaxArea(dp, y, x));
-                    }
+                    dp[y][x] = (0 < x ? dp[y][x - 1] + 1 : 1);
+                    maxArea = Math.max(maxArea, findMaxArea(dp, y, x));
                 }
             }
         }
